c32583291185f899a6d9dec37b3a43a8d6bc1d9d
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-09-14 20:12:15 +0800

    Add native manager version source

---
+++ src/BpsrRelayManager/VersionInfo.cs
@@ -0,0 +1,8 @@
+namespace BpsrRelayManager
+{
+    internal static class VersionInfo
+    {
+        public const string Version = "1.0.6";
+        public const string TestedSingBoxVersion = "v1.13.19";
+    }
+}
