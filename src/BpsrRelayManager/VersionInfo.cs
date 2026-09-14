6d861ed72547c55066d799d7d14bdb7ca7c4c704
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-09-14 20:27:50 +0800

    Set native manager version to 1.1.0

--- src/BpsrRelayManager/VersionInfo.cs
+++ src/BpsrRelayManager/VersionInfo.cs
@@ -2,7 +2,7 @@ namespace BpsrRelayManager
 {
     internal static class VersionInfo
     {
-        public const string Version = "1.0.6";
+        public const string Version = "1.1.0";
         public const string TestedSingBoxVersion = "v1.13.19";
     }
 }
